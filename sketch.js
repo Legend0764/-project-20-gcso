0771e3bd3ce8dada02760588e5565222a05dba09
Legend0764 <72258911+Legend0764@users.noreply.github.com>
2020-12-30 21:00:44 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ var speed, weight;
 
 function setup() {
   createCanvas(1600,400);
-speed=random(22,25)
+speed=random(55,90)
 weight=random(400,1500)
 
  car=createSprite(50, 200, 50, 50);
@@ -23,7 +23,7 @@ if(wall.x-car.x < (car.width+wall.width)/2)
   var deformation=0.5 * weight * speed* speed/22509;
   if(deformation>180)
   {
-    car.shapeColor=color("yellow");
+    car.shapeColor=color("red");
   }
 
   
@@ -33,7 +33,7 @@ if(wall.x-car.x < (car.width+wall.width)/2)
   
   if(deformation<80)
   {
-    car.shapeColor=color("red");
+    car.shapeColor=color("green");
   }
   }
   drawSprites();
